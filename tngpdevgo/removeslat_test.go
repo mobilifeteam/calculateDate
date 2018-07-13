cd75dcce44cafc59cf9873d0b06b15665361db40
satrawo <st.sorawat38@gmail.com>
2018-07-13 16:54:38 +0700

    [Edited] variable

--- tngpdevgo/removeslat_test.go
+++ tngpdevgo/removeslat_test.go
@@ -7,14 +7,14 @@ import (
 func Test_RemoveSlatFromDateString_Input_7_Should_be_7(t *testing.T) {
 	expectedDay, expectedMonth, expectedYear := "7", "3", "1977"
 
-	actualDay, actualMonth, actualYear := changeRemoveSlat("7/3/1997")
+	actualDay, actualMonth, actualYear := changeRemoveSlat("7/3/1977")
 	if actualDay != expectedDay {
-		t.Errorf("expected %q but go %q", expectedDay, actualDay)
+		t.Errorf("expected %s but go %s", expectedDay, actualDay)
 	}
 	if actualMonth != expectedMonth {
-		t.Errorf("expected %q but go %q", expectedDay, actualDay)
+		t.Errorf("expected %s but go %s", expectedMonth, actualMonth)
 	}
 	if actualYear != expectedYear {
-		t.Errorf("expected %q but go %q", expectedDay, actualDay)
+		t.Errorf("expected %s but go %s", expectedYear, actualYear)
 	}
 }
